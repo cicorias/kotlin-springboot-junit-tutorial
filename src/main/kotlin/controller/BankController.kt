89d82d37dc07950ba0c12093bbee7293fe315170
Shawn Cicoria <github@cicoria.com>
2022-01-17 07:42:24 -0500

    add get request to controller

--- src/main/kotlin/controller/BankController.kt
+++ src/main/kotlin/controller/BankController.kt
@@ -1,7 +1,18 @@
 package com.cicoria.kotlintutorial.thenewboston.controller
 
+import com.cicoria.kotlintutorial.thenewboston.model.Bank
+import com.cicoria.kotlintutorial.thenewboston.service.BankService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class BankController {
+@RequestMapping("api/banks")
+class BankController(private val service: BankService) {
+
+    @GetMapping
+    fun getBanks(): Collection<Bank> = service.getBanks()
+
+
+
 }
